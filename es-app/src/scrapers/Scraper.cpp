c30e70a5030ed253a67c889e6879df4dfbe46e47
acrummyidea <acrummyidea@trashmail.net>
2019-01-07 20:41:15 -0500

    Removing old scraper

--- es-app/src/scrapers/Scraper.cpp
+++ es-app/src/scrapers/Scraper.cpp
@@ -1,7 +1,6 @@
 #include "scrapers/Scraper.h"
 
 #include "FileData.h"
-#include "GamesDBScraper.h"
 #include "GamesDBJSONScraper.h"
 #include "Log.h"
 #include "Settings.h"
@@ -10,8 +9,7 @@
 #include <fstream>
 
 const std::map<std::string, generate_scraper_requests_func> scraper_request_funcs {
-  { "TheGamesDB", &thegamesdb_generate_scraper_requests },
-	{ "TheGamesDBJSON", &thegamesdb_generate_json_scraper_requests }
+	{ "TheGamesDB", &thegamesdb_generate_json_scraper_requests }
 };
 
 std::unique_ptr<ScraperSearchHandle> startScraperSearch(const ScraperSearchParams& params)
