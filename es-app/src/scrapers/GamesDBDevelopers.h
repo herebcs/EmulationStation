113d7df33f8404809b43090ab7333eeb7e68187d
acrummyidea <acrummyidea@trashmail.net>
2019-01-10 21:25:28 -0500

    Adopting the project coding style

--- es-app/src/scrapers/GamesDBDevelopers.h
+++ es-app/src/scrapers/GamesDBDevelopers.h
@@ -2,10 +2,9 @@
 #ifndef ES_APP_SCRAPERS_GAMES_DB_DEVELOPERS_H
 #define ES_APP_SCRAPERS_GAMES_DB_DEVELOPERS_H
 
-#include <string>
 #include <map>
+#include <string>
 
 extern const std::map<int, std::string> gamesdb_new_developers_map;
 
-
-#endif //ES_APP_SCRAPERS_GAMES_DB_DEVELOPERS_H
+#endif // ES_APP_SCRAPERS_GAMES_DB_DEVELOPERS_H
--- es-app/src/scrapers/GamesDBPublishers.h
+++ es-app/src/scrapers/GamesDBPublishers.h
@@ -2,10 +2,9 @@
 #ifndef ES_APP_SCRAPERS_GAMES_DB_PUBLISHERS_H
 #define ES_APP_SCRAPERS_GAMES_DB_PUBLISHERS_H
 
-#include <string>
 #include <map>
+#include <string>
 
 extern const std::map<int, std::string> gamesdb_new_publishers_map;
 
-
-#endif //ES_APP_SCRAPERS_GAMES_DB_PUBLISHERS_H
+#endif // ES_APP_SCRAPERS_GAMES_DB_PUBLISHERS_H
